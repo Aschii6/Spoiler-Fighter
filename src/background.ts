9ae71db3e36bbec17c136eba4f0440fce5c6284d
Lechea Daniel-Ioan <danutlechea2003@yahoo.com>
2025-04-18 14:48:24 +0300

    Something, something prettier.

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,5 @@
 function polling() {
-    
-    setTimeout(polling, 1000 * 30);
-  }
-  
-  polling();
-  
+  setTimeout(polling, 1000 * 30);
+}
+
+polling();
--- src/popup.tsx
+++ src/popup.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const Popup = () => {
-    return (
-        <div>
-        <h1>Welcome to the Popup!</h1>
-        <p>This is a simple popup example.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to the Popup!</h1>
+      <p>This is a simple popup example.</p>
+    </div>
+  );
 };
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
-    <React.StrictMode>
-        <Popup />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
 );
