a3ce6da4fd2cd12bfec1109249684f8cfdaadb02
Lechea Daniel-Ioan <danutlechea2003@yahoo.com>
2025-04-21 19:52:16 +0300

    OK! Added Tailwinds Css to this Webpack abomination

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import "./styles/tailwind.css";
+
 const Popup = () => {
     return (
-        <div>
+        <div className={"w-[400px] h-[400px] "}>
         <h1>Welcome to the Popup!</h1>
         <p>This is a simple popup example.</p>
         </div>
